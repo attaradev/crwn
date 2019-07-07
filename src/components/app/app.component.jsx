13a336592806d4c77640aabd0db4f711ade4028a
Mike Perry Y Attara <mpyebattara@gmail.com>
2019-07-07 18:03:21 +0000

    project start

---
+++ src/components/app/app.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.style.scss';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        The start
+      </header>
+    </div>
+  );
+};
