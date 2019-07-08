d44f7b6296ad418d919b78de5ff22c15d02621fe
Mike Perry Y Attara <mpyebattara@gmail.com>
2019-07-08 10:20:16 +0000

    add routing

--- src/components/app/app.component.jsx
+++ src/components/app/app.component.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './app.style.scss';
-import { Homepage } from '../../pages/homepage/homepage.component';
+import { Homepage } from './../../pages/homepage/homepage.component';
+import { HatsPage } from './../../pages/hats-page/hats-page.component';
 
 export const App = () => {
   return (
     <div className="app">
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 };
---
+++ src/pages/hats-page/hats-page.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const HatsPage = () => (
+  <div>
+    <h1>Hats page</h1>
+  </div>
+);
