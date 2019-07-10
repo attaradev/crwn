0e0677ec0e5cec6e3faad44c9420cf3cd84e58a0
Mike Perry Y Attara <mpyebattara@gmail.com>
2019-07-10 18:34:30 +0000

    implement signup

--- src/pages/sign-up/sign-up.component.jsx
+++ src/pages/sign-up/sign-up.component.jsx
@@ -3,10 +3,13 @@ import { Link } from 'react-router-dom';
 import './sign-up.styles.scss';
 import { FormInput } from '../../components/form-input/form-input.component';
 import { Button } from '../../components/button/button.component';
+import { auth, createUserProfileDocument } from '../../utils/firebase.utils';
 
 const INITIAL_STATE = {
+  displayName: '',
   email: '',
-  password: ''
+  password: '',
+  confirmPassword: ''
 };
 
 export class SignUpPage extends React.Component {
@@ -21,9 +24,23 @@ export class SignUpPage extends React.Component {
     this.setState({ [name]: value });
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    this.setState(INITIAL_STATE);
+    const { displayName, email, password, confirmPassword } = this.state;
+
+    if (password !== confirmPassword) {
+      alert(`Passwords don't match`);
+      return;
+    }
+
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(email, password);
+      createUserProfileDocument(user, { displayName });
+
+      this.setState(INITIAL_STATE);
+    } catch (error) {
+      console.error('Unable to register user:', error.message);
+    }
   }
 
   render() {
@@ -34,6 +51,14 @@ export class SignUpPage extends React.Component {
         <form className='form' onSubmit={this.handleSubmit}>
           <span>Already have an account? <Link to='/login'>Sign in</Link></span>
 
+          <FormInput
+            type='text'
+            name='displayName'
+            value={this.state.displayName}
+            label='name'
+            onChange={this.handleChange}
+            required
+          />
           <FormInput
             type='email'
             name='email'
@@ -47,6 +72,14 @@ export class SignUpPage extends React.Component {
             name='password'
             value={this.state.password}
             label='password'
+            onChange={this.handleChange}
+            required
+          />
+          <FormInput
+            type='password'
+            name='confirmPassword'
+            value={this.state.confirmPassword}
+            label='confirm password'
             onChange={this.handleChange}
             required
           />
