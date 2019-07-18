e3d0c4b3367978d9a94856b870ca6897c7f2b3f7
Mike Perry Y Attara <mpyebattara@gmail.com>
2019-07-18 03:34:57 +0000

    rename types

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,4 +1,4 @@
-import { Types } from './user.actions';
+import { UserTypes } from './user.actions';
 
 const INITIAL_STATE = {
   currentUser: null
@@ -6,7 +6,7 @@ const INITIAL_STATE = {
 
 export const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case Types.SET_CURRENT_USER:
+    case UserTypes.SET_CURRENT_USER:
       return { state, currentUser: action.payload };
     default:
       return state;
