1f05c51b2d1acd8cff341130dbb0e40d9420bd4f
Mike Perry Y Attara <mpyebattara@gmail.com>
2019-07-18 23:17:21 +0000

    update data ids

--- src/utils/cart.utils.js
+++ src/utils/cart.utils.js
@@ -2,11 +2,10 @@ export const addItemToCart = (itemsInCart, itemToAdd) => {
   const existingItem = itemsInCart.find(item => item.id === itemToAdd.id);
 
   if (existingItem) {
-    return itemsInCart.map(itemInCart => {
-      return itemInCart.id === itemToAdd.id
-        ? { ...itemInCart, quantity: itemInCart.quantity + 1 }
-        : itemInCart
-    });
+    return itemsInCart.map(itemInCart => itemInCart.id === itemToAdd.id
+      ? { ...itemInCart, quantity: itemInCart.quantity + 1 }
+      : itemInCart
+    );
   }
 
   return [...itemsInCart, { ...itemToAdd, quantity: 1 }];
