bf8408ea1701d485b583a82dae593b8fdc4c9223
Mike Perry Y Attara <mpyebattara@gmail.com>
2019-07-18 16:53:35 +0000

    implement add to cart

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,7 +1,13 @@
 export const CartTypes = {
-  TOGGLE_CART_HIDDEN: "TOGGLE_CART_HIDDEN"
+  TOGGLE_CART_HIDDEN: "TOGGLE_CART_HIDDEN",
+  ADD_ITEM_TO_CART: 'ADD_ITEM_TO_CART'
 };
 
 export const toggleCartHidden = () => ({
   type: CartTypes.TOGGLE_CART_HIDDEN
+});
+
+export const addItem = item => ({
+  type: CartTypes.ADD_ITEM_TO_CART,
+  payload: item
 });
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,13 +1,16 @@
 import { CartTypes } from './cart.actions';
 
 const INITIAL_STATE = {
-  hidden: true
+  hidden: true,
+  itemsInCart: []
 };
 
 export const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case CartTypes.TOGGLE_CART_HIDDEN:
       return { ...state, hidden: !state.hidden };
+    case CartTypes.ADD_ITEM_TO_CART:
+      return {...state, itemsInCart: state.itemsInCart.push(action.payload)}
     default:
       return state;
   }
