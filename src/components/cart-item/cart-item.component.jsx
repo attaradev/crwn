73c36ad495f9feba13553f40291b6122247f89a0
Mike Perry Y Attara <mpyebattara@gmail.com>
2019-07-18 23:57:14 +0000

    update cart-item styles

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -4,9 +4,7 @@ import './cart-item.styles.scss';
 
 export const CartItem = ({ item: { imageUrl, name, price, quantity } }) => (
   <div className='cart-item'>
-    <div className='image'>
-      <img src={imageUrl} alt={name} />
-    </div>
+    <img src={imageUrl} alt={name} />
     <div className='item-details'>
       <span className='name'>
         {name}
