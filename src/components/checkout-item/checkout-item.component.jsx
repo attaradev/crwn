28f1bc09cdd412409227b69c59bda94961bfbbff
Mike Perry Y Attara <mpyebattara@gmail.com>
2019-07-20 17:11:28 +0000

    create cart-item

---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './checkout-item.styles.scss';
+
+export const CheckoutItem = ({ id, name, imageUrl, price, quantity }) => (
+  <tr>
+    <td>
+      <img src={imageUrl} alt={name} />
+    </td>
+    <td>{name}</td>
+    <td>{quantity}</td>
+    <td>{price}</td>
+    <td>{`x ${id}`}</td>
+  </tr>
+)
