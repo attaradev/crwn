f5d044095d1b52e1ab571ed8797b21e1ae872580
Mike Perry Y Attara <mpyebattara@gmail.com>
2019-07-22 20:42:05 +0000

    create shop reducer

---
+++ src/redux/shop/shop.reducer.js
@@ -0,0 +1,10 @@
+import { collections } from '../../data';
+
+const INITIAL_STATE = { collections };
+
+export const shopReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
