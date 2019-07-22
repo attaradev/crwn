43cffb5d30d19e5f599f9831f68ecd3e854c5e57
Mike Perry Y Attara <mpyebattara@gmail.com>
2019-07-22 20:37:37 +0000

    refactor collection preview

--- src/components/collections-preview/collections-preview.component.jsx
+++ src/components/collections-preview/collections-preview.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import './collections-preview.scss';
 import { CollectionItem } from '../collection-item/collection-item.component';
+import './collections-preview.scss';
 
 export const CollectionsPreview = ({ title, items }) => (
   <div className='collection-preview'>
