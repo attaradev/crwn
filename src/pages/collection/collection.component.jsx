8ad4df0a26c87fc83bad985a2a8ac22729819d00
Mike Perry Y Attara <mpyebattara@gmail.com>
2019-07-22 23:45:21 +0000

    collection page

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { CollectionItem } from '../../components/collection-item/collection-item.component';
+import { selectCollection } from '../../redux/shop/shop.selectors';
 import './collection.styles.scss';
 
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionName)(state)
+})
 
-export const CollectionPage = ({ match }) => {
-  const { collectionName } = match.params;
 
-  return (
+export const CollectionPage = connect(mapStateToProps)(
+  ({ collection: { title, items } }) => (
     <div className='category'>
-      <h2>Category</h2>
+      <h2>{title}</h2>
+      {
+        items.map(item => <CollectionItem key={item.id} item={item} />)
+      }
     </div>
   )
-};
+);
