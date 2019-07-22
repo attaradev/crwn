0117cbc6bedbcd6c41ff6758ca0f04d5002d69a1
Mike Perry Y Attara <mpyebattara@gmail.com>
2019-07-22 20:45:40 +0000

    create shop selectors

--- src/pages/shop/shop.selectors.js
+++
@@ -1,8 +0,0 @@
-import { createSelector } from 'reselect';
-
-const selectShop = state => state.shop;
-
-export const selectCollections = createSelector(
-  [selectShop],
-  shop => shop.collections
-);
