71fe56aea0198d5b034182737fba9d3701ca5618
Mike Perry Y Attara <mpyebattara@gmail.com>
2019-07-23 21:50:46 +0000

    convert sign-up to styled-component

---
+++ src/pages/sign-up/sign-up.styles.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const SignUpContainer = styled.div`
+  width: 95vw;
+  max-width: 25rem;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  margin: 0 auto;
+`;
+
+
+export const Title = styled.div`
+  margin: 0.8rem 0;
+  text-transform: uppercase;
+`;
