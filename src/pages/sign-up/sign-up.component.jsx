56c4b3e984f6a4a7cf8a9b6582380cfed355168e
Mike Perry Y Attara <mpyebattara@gmail.com>
2019-07-23 08:49:47 +0000

    refactor signUpPage to hooks

--- src/pages/sign-up/sign-up.component.jsx
+++ src/pages/sign-up/sign-up.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './sign-up.styles.scss';
 import { FormInput } from '../../components/form-input/form-input.component';
@@ -12,21 +12,17 @@ const INITIAL_STATE = {
   confirmPassword: ''
 };
 
-export class SignUpPage extends React.Component {
-  constructor() {
-    super();
+export const SignUpPage = () => {
+  const [state, setState] = useState(INITIAL_STATE);
 
-    this.state = INITIAL_STATE;
-  }
-
-  handleChange = event => {
+  const handleChange = event => {
     const { name, value } = event.target;
-    this.setState({ [name]: value });
+    setState({ ...state, [name]: value });
   }
 
-  handleSubmit = async (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    const { displayName, email, password, confirmPassword } = this.state;
+    const { displayName, email, password, confirmPassword } = state;
 
     if (password !== confirmPassword) {
       alert(`Passwords don't match`);
@@ -37,55 +33,53 @@ export class SignUpPage extends React.Component {
       const { user } = await auth.createUserWithEmailAndPassword(email, password);
       createUserProfileDocument(user, { displayName });
 
-      this.setState(INITIAL_STATE);
+      setState(INITIAL_STATE);
     } catch (error) {
       alert(error.message);
     }
   }
 
-  render() {
-    return (
-      <div className='sign-up'>
-        <h2 className='title'>Sign in to your account</h2>
-        <span>Sign up with your email and password</span>
-        <form className='form' onSubmit={this.handleSubmit}>
-          <span>Already have an account? <Link to='/login'>Sign in</Link></span>
+  return (
+    <div className='sign-up'>
+      <h2 className='title'>Sign in to your account</h2>
+      <span>Sign up with your email and password</span>
+      <form className='form' onSubmit={handleSubmit}>
+        <span>Already have an account? <Link to='/login'>Sign in</Link></span>
 
-          <FormInput
-            type='text'
-            name='displayName'
-            value={this.state.displayName}
-            label='name'
-            onChange={this.handleChange}
-            required
-          />
-          <FormInput
-            type='email'
-            name='email'
-            value={this.state.email}
-            label='email'
-            onChange={this.handleChange}
-            required
-          />
-          <FormInput
-            type='password'
-            name='password'
-            value={this.state.password}
-            label='password'
-            onChange={this.handleChange}
-            required
-          />
-          <FormInput
-            type='password'
-            name='confirmPassword'
-            value={this.state.confirmPassword}
-            label='confirm password'
-            onChange={this.handleChange}
-            required
-          />
-          <Button style={{ width: '100%' }} type='submit'>Sign up</Button>
-        </form>
-      </div>
-    );
-  }
+        <FormInput
+          type='text'
+          name='displayName'
+          value={state.displayName}
+          label='name'
+          onChange={handleChange}
+          required
+        />
+        <FormInput
+          type='email'
+          name='email'
+          value={state.email}
+          label='email'
+          onChange={handleChange}
+          required
+        />
+        <FormInput
+          type='password'
+          name='password'
+          value={state.password}
+          label='password'
+          onChange={handleChange}
+          required
+        />
+        <FormInput
+          type='password'
+          name='confirmPassword'
+          value={state.confirmPassword}
+          label='confirm password'
+          onChange={handleChange}
+          required
+        />
+        <Button style={{ width: '100%' }} type='submit'>Sign up</Button>
+      </form>
+    </div>
+  );
 }
