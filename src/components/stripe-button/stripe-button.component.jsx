68c7c887d223f88eedd93f9282261a55d6912555
Mike Perry Y Attara <mpyebattara@gmail.com>
2019-07-23 12:07:14 +0000

    clear cart after successful payment

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,29 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import StripeCheckout from 'react-stripe-checkout';
 import { formatAsMoney } from '../../utils/cart.utils';
+import { clearCart } from '../../redux/cart/cart.actions';
 import './stripe-button.styles.scss';
 
-export const StripeCheckoutButton = ({ price }) => {
-  const priceForStripe = price * 100;
-  const publishableKey = process.env.REACT_APP_STRIPE_API_KEY;
 
-  const onToken = token => {
-    console.log(token);
-    alert('Payment Successful');
-  };
 
-  return (
-    <StripeCheckout
-      label='Pay Now'
-      name='CRWN, Inc'
-      billingAddress
-      shippingAddress
-      image='https://svgshare.com/i/CUz.svg'
-      description={`Your cart total is ${formatAsMoney(price)}`}
-      amount={priceForStripe}
-      panelLabel='Pay Now'
-      token={onToken}
-      stripeKey={publishableKey}
-    />
-  );
-}
+
+export const StripeCheckoutButton = connect()(
+  ({ price, dispatch }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = process.env.REACT_APP_STRIPE_API_KEY;
+
+    const onToken = token => {
+      console.log(token);
+      dispatch(clearCart());
+      alert('Payment Successful');
+    };
+
+    return (
+      <StripeCheckout
+        label='Pay Now'
+        name='CRWN, Inc'
+        billingAddress
+        shippingAddress
+        image='https://svgshare.com/i/CUz.svg'
+        description={`Your cart total is ${formatAsMoney(price)}`}
+        amount={priceForStripe}
+        panelLabel='Pay Now'
+        token={onToken}
+        stripeKey={publishableKey}
+      />
+    );
+  }
+)
