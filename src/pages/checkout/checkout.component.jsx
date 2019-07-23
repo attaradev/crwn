3e9c882147e93544e9c8b4e1fe9bcb15dc5e8f01
Mike Perry Y Attara <mpyebattara@gmail.com>
2019-07-23 18:43:14 +0000

    change checkout to styled-component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -6,7 +6,13 @@ import { StripeCheckoutButton } from '../../components/stripe-button/stripe-butt
 import { selectItemsInCart, selectCartTotal } from '../../redux/cart/cart.selectors';
 import { removeItemFromCart, decreaseItem, increaseItem } from '../../redux/cart/cart.actions';
 import { formatAsMoney } from '../../utils/cart.utils';
-import './checkout.styles.scss';
+import {
+  CheckoutContainer,
+  CheckoutHeader,
+  HeaderBlock,
+  TotalBlock,
+  WarningText
+} from './checkout.styles';
 
 
 const mapStateToProps = createStructuredSelector({
@@ -23,40 +29,46 @@ const mapDispatchToProps = dispatch => ({
 export const CheckoutPage = connect(
   mapStateToProps,
   mapDispatchToProps
-)(({ itemsInCart, cartTotal, removeItemFromCart, increaseItem, decreaseItem }) => (
-  <div className='checkout'>
-    <div className='checkout-header'>
-      <div className='header-block'>
-        <span>Product</span>
-      </div>
-      <div className='header-block'>
-        <span>Description</span>
-      </div>
-      <div className='header-block'>
-        <span>Quantity</span>
-      </div>
-      <div className='header-block'>
-        <span>Price</span>
-      </div>
-      <div className='header-block'>
-        <span>Remove</span>
-      </div>
-    </div>
-    {itemsInCart.map(cartItem => (
-      <CheckoutItem
-        key={cartItem.id}
-        item={cartItem}
-        handleDelete={removeItemFromCart}
-        handleIncrease={increaseItem}
-        handleDecrease={decreaseItem}
-      />
-    ))}
-    <div className='total'>TOTAL: {formatAsMoney(cartTotal)}</div>
-    <div className='test-warning'>
-      **Please use the following test credit card for checkout.
+)(({
+  itemsInCart,
+  cartTotal,
+  removeItemFromCart,
+  increaseItem,
+  decreaseItem
+}) => (
+    <CheckoutContainer>
+      <CheckoutHeader>
+        <HeaderBlock>
+          <span>Product</span>
+        </HeaderBlock>
+        <HeaderBlock>
+          <span>Description</span>
+        </HeaderBlock>
+        <HeaderBlock>
+          <span>Quantity</span>
+        </HeaderBlock>
+        <HeaderBlock>
+          <span>Price</span>
+        </HeaderBlock>
+        <HeaderBlock>
+          <span>Remove</span>
+        </HeaderBlock>
+      </CheckoutHeader>
+      {itemsInCart.map(cartItem => (
+        <CheckoutItem
+          key={cartItem.id}
+          item={cartItem}
+          handleDelete={removeItemFromCart}
+          handleIncrease={increaseItem}
+          handleDecrease={decreaseItem}
+        />
+      ))}
+      <TotalBlock>TOTAL: {formatAsMoney(cartTotal)}</TotalBlock>
+      <WarningText>
+        **Please use the following test credit card for checkout.
       <br />
-      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
-    </div>
-    <StripeCheckoutButton price={cartTotal} />
-  </div>
-));
+        4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+    </WarningText>
+      <StripeCheckoutButton price={cartTotal} />
+    </CheckoutContainer>
+  ));
