d1c6b4061c26c58e4360e1b22464e47fad378d8c
Mike Perry Y Attara <mpyebattara@gmail.com>
2019-07-23 21:35:15 +0000

    convert shop to styled-component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,18 +2,23 @@ import React from 'react';
 import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 import { Route } from 'react-router-dom';
+import styled from 'styled-components';
 import { selectCollectionsForPreview } from './../../redux/shop/shop.selectors';
 import { CollectionPage } from '../collection/collection.component';
 import { CollectionsOverview } from '../../components/collections-overview/collections-overview.component';
-import './shop.styles.scss';
+
+const ShopContainer = styled.div`
+  margin-bottom: 6rem;
+  min-height: 100%;
+`;
 
 const mapStateToProps = createStructuredSelector({
   collections: selectCollectionsForPreview
 });
 
 export const ShopPage = connect(mapStateToProps)(({ match }) => (
-  <div className='shop'>
+  <ShopContainer>
     <Route exact path={`${match.path}`} component={CollectionsOverview} />
     <Route path={`${match.path}/:collectionName`} component={CollectionPage} />
-  </div>
+  </ShopContainer>
 ));
