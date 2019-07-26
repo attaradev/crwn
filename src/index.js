33d61952d0472ed4adcceb2e894de870cf8d2e5e
Mike Perry Y Attara <mpyebattara@gmail.com>
2019-07-26 22:19:03 +0000

    update project

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { PersistGate } from 'redux-persist/integration/react';
 import App from './app/app.component';
 import { store, persistor } from './redux/store';
 import * as serviceWorker from './serviceWorker';
-import './index.style.css';
 
 
 ReactDOM.render(
