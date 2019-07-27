75bf65d696a073d4042f44320c6c5d993f06804d
Mike Perry Y Attara <mpyebattara@gmail.com>
2019-07-27 01:53:55 +0000

    implement asyc data collection

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,8 +1,39 @@
+import {
+  firestore,
+  convertCollectionsSnapshotToMap
+} from '../../utils/firebase.utils';
+
 export const ShopTypes = {
-  UPDATE_COLLECTIONS: 'UPDATE_COLLECTIONS'
+  FETCH_COLLECTIONS_START: 'FETCH_COLLECTIONS_START',
+  FETCH_COLLECTIONS_SUCCESS: 'FETCH_COLLECTIONS_SUCCESS',
+  FETCH_COLLECTIONS_FAIL: 'FETCH_COLLECTIONS_FAIL'
 };
 
-export const updateCollections = collections => ({
-  type: ShopTypes.UPDATE_COLLECTIONS,
+export const fetchCollectionsStart = () => ({
+  type: ShopTypes.FETCH_COLLECTIONS_START
+});
+
+export const fetchCollectionsSuccess = collections => ({
+  type: ShopTypes.FETCH_COLLECTIONS_SUCCESS,
   payload: collections
 });
+
+export const fetchCollectionsFail = error => ({
+  type: ShopTypes.FETCH_COLLECTIONS_SUCCESS,
+  payload: error
+});
+
+export const fetchCollectionsAsync = () => {
+  return dispatch => {
+    const collectionReference = firestore.collection('collections');
+    dispatch(fetchCollectionsStart());
+
+    collectionReference
+      .get()
+      .then(snapshot => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        dispatch(fetchCollectionsSuccess(collectionsMap));
+      })
+      .catch(error => dispatch(fetchCollectionsFail(error.message)));
+  };
+};
--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,11 +1,19 @@
 import { ShopTypes } from './shop.actions';
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+  collections: null,
+  isFetching: false,
+  error: undefined
+};
 
 export const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case ShopTypes.UPDATE_COLLECTIONS:
-      return { state, collections: action.payload }
+    case ShopTypes.FETCH_COLLECTIONS_START:
+      return { ...state, isFetching: true }
+    case ShopTypes.FETCH_COLLECTIONS_SUCCESS:
+      return { ...state, isFetching: false, collections: action.payload }
+    case ShopTypes.FETCH_COLLECTIONS_FAIL:
+      return { ...state, isFetching: false, error: action.payload }
     default:
       return state;
   }
