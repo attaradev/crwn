f2b5cf349426d7279d2a6f96dc6d71b5f18ed475
Mike Perry Y Attara <mpyebattara@gmail.com>
2019-08-13 06:21:23 +0000

    move frontent to client dir

--- src/app/app.test.js
+++
@@ -1,7 +0,0 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './app.component';
-
-test('sample test', () => {
-  expect(true).toBe(true)
-});
--- src/components/with-spinner/with-spinner.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Spinner } from '../spinner/spinner.component';
-
-export const WithSpinner = Component => ({ isLoading, ...props }) => (
-  isLoading
-    ? <Spinner />
-    : <Component {...props} />
-
-);
